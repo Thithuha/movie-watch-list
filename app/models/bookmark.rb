a5114b115104eb1ca7a56ba412539785dd893bb4
thithuha nguyen <chloe.nguyen1910@gmail.com>
2021-09-21 12:29:28 +0200

    validates uniqueness for movie/list by scope

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,6 +1,7 @@
 class Bookmark < ApplicationRecord
-  validates :comment, length: { minimum: 6 }
   belongs_to :movie
   belongs_to :list
 
+  validates :comment, length: { minimum: 6 }
+  validates :movie_id, uniqueness: { scope: :list_id, message: "is in the list" }
 end
